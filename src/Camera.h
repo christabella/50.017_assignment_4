2022d6a4eba6454ff8caa072dd98b99794442cec
Christabella Irwanto <christabella.irwanto@gmail.com>
2017-03-27 11:25:56 +0800

    fill in perspectiveCamera

--- src/Camera.h
+++ src/Camera.h
@@ -16,10 +16,10 @@ public:
 	virtual float getTMin() const = 0 ; 
 	virtual ~Camera(){}
 protected:
-	Vector3f center; 
-	Vector3f direction;
-	Vector3f up;
-	Vector3f horizontal;
+	Vector3f m_center;
+	Vector3f m_direction;
+	Vector3f m_up;
+	Vector3f m_horizontal;
 
 };
 
@@ -29,11 +29,28 @@ class PerspectiveCamera: public Camera
 {
 public:
 	PerspectiveCamera(const Vector3f& center, const Vector3f& direction,const Vector3f& up , float angle){
-
+        m_center = center;
+        m_direction = direction;
+        m_up = up;
+        m_angle = angle;
 	}
 
 	virtual Ray generateRay( const Vector2f& point){
-		return Ray(Vector3f(1,0,0),Vector3f(1,0,0));
+        float x = point[0];
+        float y = point[1];
+        float D = 1.0/tan(m_angle / 2.0);
+        
+        Vector3f w = m_direction.normalized();
+        Vector3f u = (Vector3f::cross(w, m_up)).normalized();
+        Vector3f v = (Vector3f::cross(u, w)).normalized();
+        
+        // direction
+        
+        Vector3f r = x*u + y*v + D*w;
+        
+        Vector3f d = r.normalized();
+        
+		return Ray(m_center, d);
 	}
 
 	virtual float getTMin() const { 
@@ -41,6 +58,7 @@ public:
 	}
 
 private:
+    float m_angle;
 
 };
 
